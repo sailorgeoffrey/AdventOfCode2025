7e7a88b4a04ebd8301533d15f31e69ac869dd7d9
Geoffrey Chandler <geoffc@gmail.com>
2025-12-02 12:54:34 +0100

    show both parts

--- src/main/kotlin/io/gcc/adventofcode/GiftShop.kt
+++ src/main/kotlin/io/gcc/adventofcode/GiftShop.kt
@@ -7,8 +7,14 @@ import java.io.File
  * https://adventofcode.com/2025/day/2
  */
 object GiftShop {
+    fun invalidByRepeats(test: Long): Boolean {
+        val str = test.toString()
+        if (str.length % 2 == 1) return false
+        if (str.take(str.length / 2) == str.substring(str.length / 2)) return true
+        return false
+    }
 
-    fun isInvalid(test: Long): Boolean {
+    fun invalidBySequence(test: Long): Boolean {
         val str = test.toString()
         val length = str.length
         val factors = (1..length).filter { length % it == 0 }.dropLast(1)
@@ -19,17 +25,19 @@ object GiftShop {
         return false
     }
 
-    fun filterForInvalid(input: String): List<Long> = input.lines()
+    fun filterForInvalid(input: String, function: (test: Long) -> Boolean): List<Long> = input.lines()
         .joinToString("")
         .split(',')
         .map { it.split('-') }
         .map { (start, end) -> LongRange(start.toLong(), end.toLong()) }
-        .flatMap { range -> range.filter { isInvalid(it) } }
+        .flatMap { range -> range.filter(function) }
 
-    fun sumInvalid(input: String): Long = filterForInvalid(input).sum()
+    fun sumInvalid(input: String, function: (test: Long) -> Boolean): Long = filterForInvalid(input, function).sum()
+
 }
 
 fun main() {
     val input = File("src/main/resources/gift-shop-input.txt").readText()
-    println("The sum of all invalid codes is ${GiftShop.sumInvalid(input)}")
+    println("The sum of all invalid codes for part 1 is ${GiftShop.sumInvalid(input, GiftShop::invalidByRepeats)}")
+    println("The sum of all invalid codes for part 2 is ${GiftShop.sumInvalid(input, GiftShop::invalidBySequence)}")
 }
--- src/test/kotlin/io/gcc/adventofcode/GiftShopTest.kt
+++ src/test/kotlin/io/gcc/adventofcode/GiftShopTest.kt
@@ -17,22 +17,42 @@ class GiftShopTest {
     }
 
     @Test
-    fun testIsInvalid() {
-        assertTrue(GiftShop.isInvalid(11))
-        assertTrue(GiftShop.isInvalid(55))
-        assertTrue(GiftShop.isInvalid(6464))
-        assertTrue(GiftShop.isInvalid(123123))
-        assertFalse(GiftShop.isInvalid(12))
-        assertFalse(GiftShop.isInvalid(101))
-        assertTrue(GiftShop.isInvalid(12341234))
-        assertFalse(GiftShop.isInvalid(12))
-        assertTrue(GiftShop.isInvalid(123123123))
-        assertTrue(GiftShop.isInvalid(1212121212))
-        assertTrue(GiftShop.isInvalid(1111111))
+    fun testInvalidByRepeats() {
+        assertTrue(GiftShop.invalidByRepeats(11))
+        assertTrue(GiftShop.invalidByRepeats(55))
+        assertTrue(GiftShop.invalidByRepeats(6464))
+        assertTrue(GiftShop.invalidByRepeats(123123))
+        assertFalse(GiftShop.invalidByRepeats(12))
+        assertFalse(GiftShop.invalidByRepeats(101))
     }
 
     @Test
-    fun testFilter() {
+    fun testInvalidBySequence() {
+        assertTrue(GiftShop.invalidBySequence(12341234))
+        assertFalse(GiftShop.invalidBySequence(12))
+        assertTrue(GiftShop.invalidBySequence(123123123))
+        assertTrue(GiftShop.invalidBySequence(1212121212))
+        assertTrue(GiftShop.invalidBySequence(1111111))
+    }
+
+    @Test
+    fun testFilterPart1() {
+        assertEquals(
+            listOf(
+                11L, 22L,
+                99L,
+                1010L,
+                1188511885L,
+                222222L,
+                446446L,
+                38593859L,
+            ),
+            GiftShop.filterForInvalid(input, GiftShop::invalidByRepeats)
+        )
+    }
+
+    @Test
+    fun testFilterPart2() {
         assertEquals(
             listOf(
                 11L, 22L,
@@ -46,13 +66,13 @@ class GiftShopTest {
                 824824824L,
                 2121212121L
             ),
-            GiftShop.filterForInvalid(input)
+            GiftShop.filterForInvalid(input, GiftShop::invalidBySequence)
         )
     }
 
     @Test
     fun testSum() {
-        assertEquals(4174379265, GiftShop.sumInvalid(input))
+        assertEquals(4174379265, GiftShop.sumInvalid(input, GiftShop::invalidBySequence))
     }
 
 }
