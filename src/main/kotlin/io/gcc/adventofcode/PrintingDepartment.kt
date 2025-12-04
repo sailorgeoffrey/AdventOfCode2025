c194b4071cb36d7856af175233f30d720290cb6b
Geoffrey Chandler <geoffc@gmail.com>
2025-12-04 15:35:19 +0100

    golf (but with tests)

--- src/main/kotlin/io/gcc/adventofcode/PrintingDepartment.kt
+++ src/main/kotlin/io/gcc/adventofcode/PrintingDepartment.kt
@@ -4,30 +4,18 @@ import java.io.File
 
 class PrintingDepartment(input: String) {
 
-    val grid: Array<Array<Boolean>> =
-        input.lines().let { lines -> Array(lines.size) { y -> Array(lines[y].length) { x -> lines[y][x] == '@' } } }
+    val g = input.lines().let { l -> Array(l.size) { y -> Array(l[y].length) { l[y][it] == '@' } } }
 
-    override fun toString(): String = grid.joinToString("\n") { row -> row.joinToString("") { if (it) "@" else "." } }
+    override fun toString() = g.joinToString("\n") { it.joinToString("") { if (it) "@" else "." } }
+    fun countAdjacent(y: Int, x: Int) =
+        (-1..1).sumOf { a -> (-1..1)
+            .count { b -> (a != 0 || b != 0) && y + a in g.indices && x + b in g[0].indices && g[y + a][x + b] } }
 
-    fun countAdjacent(y: Int, x: Int): Int = (y - 1..y + 1).sumOf { dy ->
-        (x - 1..x + 1).count { dx ->
-            !(dy == y && dx == x) &&
-                    dy in grid.indices &&
-                    dx in grid[0].indices &&
-                    grid[dy][dx]
-        }
-    }
+    fun findAccessible() =
+        g.indices.flatMap { y -> g[0].indices.filter { g[y][it] && countAdjacent(y, it) < 4 }.map { y to it } }
 
-    fun findAccessible(): List<Pair<Int, Int>> = grid.indices.flatMap { dy ->
-        grid[0].indices
-            .filter { dx -> grid[dy][dx] && countAdjacent(dy, dx) < 4 }
-            .map { dx -> Pair(dy, dx) }
-    }
-
-    fun removeRolls(): Int = generateSequence { findAccessible().takeIf { it.isNotEmpty() } }
-        .flatMap { it.asSequence() }
-        .onEach { (y, x) -> grid[y][x] = false }
-        .count()
+    fun removeRolls() = generateSequence { findAccessible().takeIf { it.isNotEmpty() } }.flatMap { it.asSequence() }
+        .onEach { (y, x) -> g[y][x] = false }.count()
 }
 
 fun main() {
