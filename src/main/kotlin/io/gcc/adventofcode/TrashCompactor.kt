04e62780d56738e308d62f5cd884e5e29b243d60
Geoffrey Chandler <geoffc@gmail.com>
2025-12-06 19:36:21 +0100

    6 dec. part 2

--- src/main/kotlin/io/gcc/adventofcode/TrashCompactor.kt
+++ src/main/kotlin/io/gcc/adventofcode/TrashCompactor.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 object TrashCompactor {
 
-    data class ProblemInput(val operator: Operator, val operands: MutableList<Long> = mutableListOf()) {
+    data class ProblemInput(val operator: Operator, val operands: MutableList<String> = mutableListOf()) {
         enum class Operator {
             ADD, MULTIPLY;
 
@@ -20,21 +20,50 @@ object TrashCompactor {
 
     fun readAndTranspose(input: File): Array<ProblemInput> {
         val lines = input.readLines()
+        val lastLine = lines.last()
         val operators = lines.last().split("\\s+".toRegex()).filter { it.isNotBlank() }.map { it.first() }
+        val cols = lastLine.split("(\\S)".toRegex()).toMutableList()
+        cols[cols.size - 1] = cols[cols.size - 1] + " "
+        val colWidths = cols.map { it.length }.drop(1)
         val problems = Array(operators.size) {
             ProblemInput(ProblemInput.Operator.of(operators[it]))
         }
         lines.forEachIndexed { index, line ->
             if (index == lines.size - 1) return@forEachIndexed
-            val tokens = line.split("\\s+".toRegex()).filter { it.isNotBlank() }
-            tokens.forEachIndexed { tokenIndex, token -> problems[tokenIndex].operands.add(token.toLong()) }
+            val tokens: MutableList<String> = mutableListOf()
+            var cursor = 0
+            colWidths.forEach { width ->
+                tokens.add(line.substring(cursor, cursor + width))
+                cursor += width + 1
+            }
+            tokens.forEachIndexed { tokenIndex, token -> problems[tokenIndex].operands.add(token) }
         }
         return problems
     }
 
-    fun compute(input: ProblemInput): Long = when (input.operator) {
-        ProblemInput.Operator.ADD -> input.operands.sumOf { it }
-        ProblemInput.Operator.MULTIPLY -> input.operands.fold(initial = 1) { acc, num -> acc * num }
+    fun computePart1(input: ProblemInput): Long = when (input.operator) {
+        ProblemInput.Operator.ADD -> input.operands.map { it.trim().toLong() }.sumOf { it }
+        ProblemInput.Operator.MULTIPLY -> input.operands.map { it.trim().toLong() }
+            .fold(initial = 1) { acc, num -> acc * num }
+    }
+
+    fun computePart2(input: ProblemInput): Long {
+        val values = input.operands.map { it.trim().toLong() }
+        val cols = values.max().toString().length
+        var col = cols - 1
+        val operands = Array(cols) {
+            val b = StringBuilder()
+            input.operands.forEach { l ->
+                val thisChar = l.toCharArray()[col]
+                b.append(thisChar)
+            }
+            col--
+            return@Array b.toString().trim().toLong()
+        }
+        return when (input.operator) {
+            ProblemInput.Operator.ADD -> operands.sumOf { it }
+            ProblemInput.Operator.MULTIPLY -> operands.fold(initial = 1) { acc, num -> acc * num }
+        }
     }
 
 }
@@ -42,6 +71,6 @@ object TrashCompactor {
 fun main() {
     val input = File("src/main/resources/trash-compactor-input.txt")
     val problems = TrashCompactor.readAndTranspose(input)
-    val total = problems.sumOf { TrashCompactor.compute(it) }
-    println("the check total for part 1 is :$total")
+    println("the check total for part 1 is :${problems.sumOf { TrashCompactor.computePart1(it) }}")
+    println("the check total for part 2 is :${problems.sumOf { TrashCompactor.computePart2(it) }}")
 }
--- src/test/kotlin/io/gcc/adventofcode/TrashCompactorTest.kt
+++ src/test/kotlin/io/gcc/adventofcode/TrashCompactorTest.kt
@@ -13,13 +13,25 @@ class TrashCompactorTest {
         val problems = TrashCompactor.readAndTranspose(input)
         val first = problems.first()
         assertEquals(TrashCompactor.ProblemInput.Operator.MULTIPLY, first.operator)
-        assertEquals(listOf(123L, 45L, 6L), first.operands)
+        assertEquals(listOf(123L, 45L, 6L), first.operands.map { it.trim().toLong() })
     }
 
     @Test
-    fun testCompute(){
+    fun testComputePart1(){
         val problems = TrashCompactor.readAndTranspose(input)
-        assertEquals(33210L, TrashCompactor.compute(problems.first()))
+        assertEquals(33210L, TrashCompactor.computePart1(problems[0]))
+        assertEquals(490L, TrashCompactor.computePart1(problems[1]))
+        assertEquals(4243455L, TrashCompactor.computePart1(problems[2]))
+        assertEquals(401L, TrashCompactor.computePart1(problems[3]))
+    }
+
+    @Test
+    fun testComputePart2(){
+        val problems = TrashCompactor.readAndTranspose(input)
+        assertEquals(1058L, TrashCompactor.computePart2(problems[3]))
+        assertEquals(3253600L, TrashCompactor.computePart2(problems[2]))
+        assertEquals(625L, TrashCompactor.computePart2(problems[1]))
+        assertEquals(8544L, TrashCompactor.computePart2(problems[0]))
     }
 
 }
